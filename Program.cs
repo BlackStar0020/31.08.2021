8f2f39297066947d6f88161d2d875b0e4699a404
Dilya <dildoranasriddinzoda07@gmail.com>
2021-09-01 12:44:36 +0300

    first commut

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace _31._08._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            Play(player);
+        }
+        public static void Play(IpIr player)
+        {
+            player.play();
+            player.pause();
+            player.stop();
+            player.record();
+            player.pause2();
+            player.stop2();
+
+        }
+    }    
+        interface  IPlayable
+        {
+            void play();
+            void pause();  
+            void stop();
+
+        }
+        interface IRecodable
+        {
+           void record();
+           void pause2(); 
+           void stop2();
+        }
+        interface IpIr : IPlayable, IRecodable
+        {
+
+        }
+        class Player : IpIr
+        {
+            public void play()
+            {
+                Console.WriteLine("Play");
+            }
+            public void pause()
+            {
+               Console.WriteLine("Pause");
+            }
+            public void stop()
+            {
+             Console.WriteLine("Stop");       
+            }
+            public void record()
+            {
+              Console.WriteLine("Record");
+            }
+            public void pause2()
+            {
+               Console.WriteLine("Pause2");
+            }
+            public void stop2()
+            {
+             Console.WriteLine("Stop2");       
+            }
+            
+        }
+}
